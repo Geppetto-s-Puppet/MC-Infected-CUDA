41f2948c37024e1a1fc6d0807682b952805c318e
Geppetto <biha0718@gmail.com>
2026-03-22 18:18:42 +0900

    Initial commit

---
+++ src/main/java/me/alex/infected/Infected.java
@@ -0,0 +1,17 @@
+package me.alex.infected;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Infected extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
